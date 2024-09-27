d144d4679d116cc250ae2c988d1c950288891f11
rigzad <81182605+rigzad@users.noreply.github.com>
2024-09-27 18:51:49 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExTypst.MixProject do
     [
       app: :ex_typst,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
@@ -36,7 +36,7 @@ defmodule ExTypst.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.28.0"},
+      {:rustler, "~> 0.34.0"},
       {:benchee, "~> 1.0", only: :dev},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false}
